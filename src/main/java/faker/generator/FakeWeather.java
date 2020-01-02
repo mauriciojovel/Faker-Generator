ed1900beddd210d9b25cad131b79a7e636fb9708
Mauricio Jovel <mauriciojovel@gmail.com>
2020-01-02 00:35:38 -0600

    :recycle: Refactor the FakerGen to use IOC pattern

--- src/main/java/faker/generator/FakeWeather.java
+++ src/main/java/faker/generator/FakeWeather.java
@@ -1,11 +1,15 @@
 package faker.generator;
 
+import faker.generator.services.FakeServiceName;
+import faker.generator.services.FakeWeatherService;
+
 import java.lang.annotation.*;
 
 @Documented
 @Retention(value = RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 @Inherited
+@FakeServiceName(FakeWeatherService.class)
 public @interface FakeWeather {
     FakerWeatherType value() default FakerWeatherType.DESCRIPTION;
 }
---
+++ src/main/java/faker/generator/services/FakeRickAndMortyService.java
@@ -0,0 +1,13 @@
+package faker.generator.services;
+
+import com.github.javafaker.Faker;
+import faker.generator.FakeRickAndMorty;
+
+import java.lang.reflect.Field;
+
+public class FakeRickAndMortyService implements FakeService<FakeRickAndMorty> {
+    @Override
+    public void setValue(FakeRickAndMorty annotation, Field target, Object data, Faker faker) throws IllegalAccessException {
+        target.set(data, faker.resolve(annotation.value().getFakerKey()));
+    }
+}
