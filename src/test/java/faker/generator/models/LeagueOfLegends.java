f624f2df27a996191582d36a2ed0b788b1b4428d
dcruz <dumanzor@applaudostudios.com>
2020-01-02 17:42:41 -0600

    :art: Adding new structure for League of legends
    Addinf new structure

---
+++ src/test/java/faker/generator/models/LeagueOfLegends.java
@@ -0,0 +1,28 @@
+package faker.generator.models;
+
+import faker.generator.FakeLeagueOfLegends;
+import faker.generator.FakeLeagueOfLegendsType;
+import lombok.Data;
+
+@Data
+public class LeagueOfLegends {
+
+    @FakeLeagueOfLegends(FakeLeagueOfLegendsType.CHAMPION)
+    private String champion;
+
+    @FakeLeagueOfLegends(FakeLeagueOfLegendsType.LOCATION)
+    private String location;
+
+    @FakeLeagueOfLegends(FakeLeagueOfLegendsType.MASTERIES)
+    private String masteries;
+
+    @FakeLeagueOfLegends(FakeLeagueOfLegendsType.QUOTE)
+    private String quote;
+
+    @FakeLeagueOfLegends(FakeLeagueOfLegendsType.RANK)
+    private String rank;
+
+    @FakeLeagueOfLegends(FakeLeagueOfLegendsType.SUMMONERSPELL)
+    private String summonerSpell;
+
+}
