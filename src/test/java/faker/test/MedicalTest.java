842213fd7b73f62bd6505a9f7e03eece7378bba9
dcruz <dumanzor@applaudostudios.com>
2020-01-02 15:14:36 -0600

    :art: Creating a test class for Medical feature
    Creating a test class for Medical feature

---
+++ src/test/java/faker/test/MedicalTest.java
@@ -0,0 +1,25 @@
+package faker.test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import faker.generator.FakerGen;
+import faker.generator.models.*;
+
+import java.util.Locale;
+import java.util.Optional;
+import org.junit.Test;
+
+public class MedicalTest {
+    @Test
+    public void testMedical() {
+        Optional<Medical> b = FakerGen.create(Medical.class);
+        assertTrue(b.isPresent());
+        b.ifPresent(bo -> {
+            assertNotNull(bo.getMedicineName());
+            assertNotNull(bo.getDiseaseName());
+            assertNotNull(bo.getHospitalName());
+            assertNotNull(bo.getSymptom());
+        });
+    }
+}
