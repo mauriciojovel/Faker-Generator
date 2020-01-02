78b016bd96f7a127720273eb20fb31df882d76fb
dcruz <dumanzor@applaudostudios.com>
2020-01-02 16:25:21 -0600

    :art: Creating a test class for StarTrek feature
    Creating a test class for StarTrek feature

---
+++ src/test/java/faker/generator/test/StartTrekTest.java
@@ -0,0 +1,22 @@
+package faker.generator.test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import faker.generator.FakerGen;
+import faker.generator.models.*;
+import java.util.Optional;
+import org.junit.Test;
+
+public class StartTrekTest {
+    @Test
+    public void testStarTrek() {
+        Optional<StarTrek> c = FakerGen.create(StarTrek.class);
+        assertTrue(c.isPresent());
+        c.ifPresent(co -> {
+            assertNotNull(co.getCharacter());
+            assertNotNull(co.getLocation());
+            assertNotNull(co.getSpecie());
+            assertNotNull(co.getVillain());
+        });
+    }
+}
