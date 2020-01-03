c6104ab899151d77cae90b4f11913074164c8b5d
iturcios <54926582+iturcios@users.noreply.github.com>
2020-01-03 13:11:53 -0600

    :sparkles: Adding support to feature/GameOfThrones (#101)

---
+++ src/main/java/faker/generator/services/FakeGameOfThronesService.java
@@ -0,0 +1,14 @@
+package faker.generator.services;
+
+import com.github.javafaker.Faker;
+import faker.generator.FakeGameOfThrones;
+
+import java.lang.reflect.Field;
+
+public class FakeGameOfThronesService implements FakeService<FakeGameOfThrones> {
+    @Override
+    public void setValue(FakeGameOfThrones annotation, Field target, Object data, Faker faker) throws IllegalAccessException {
+        target.set(data, faker.resolve(annotation.value().getFakerKey()));
+
+    }
+}
