9ff5e04798d8d791debf631a24e31bf42f8b481f
dcruz <dumanzor@applaudostudios.com>
2020-01-06 10:57:47 -0600

    :art: Adding to support HowIMetYourMother feature with a new structure
    Adding to support HowIMetYourMother feature with a new structure

---
+++ src/main/java/faker/generator/FakeHowIMetYourMother.java
@@ -0,0 +1,21 @@
+package faker.generator;
+import faker.generator.services.FakeHowIMetYourMotherService;
+import faker.generator.services.FakeService;
+import faker.generator.services.FakeServiceName;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(value = RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@Inherited
+@FakeServiceName(FakeHowIMetYourMotherService.class)
+public @interface FakeHowIMetYourMother {
+    FakeHowIMetYourMotherType value() default FakeHowIMetYourMotherType.CHARACTER;
+
+}
